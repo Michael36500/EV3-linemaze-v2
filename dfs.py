b540706b0c5ec345c4603d196a4d09238f7eda0b
Michael36500 <ambros.michael@seznam.cz>
2024-07-10 17:03:05 +0200

    follow line method

--- dfs.py
+++ dfs.py
@@ -79,12 +79,9 @@ def get_neighbours_near(mapa: List[List[int]], pos: Pos) -> List[Pos]:
 
 def follow_line(mapa: List[List[int]], pos_from: Pos, pos_to: Pos) -> Pos:
     # ─ │ ┌ ┐ ┘ ├ └ ┤ ┬ ┴ ┼ ▫ ■
-    print(f'from: {pos_from} to: {pos_to}')
-    print(f'from: {mapa[pos_from.y][pos_from.x]} to: {mapa[pos_to.y][pos_to.x]}')
-
     # if there is not a need to follow a line
-    if mapa[pos_to.y][pos_to.x] in '├ ┤ ┬ ┴ ┼ ▫ ■':
-        return pos_to
+    # if mapa[pos_to.y][pos_to.x] in '├ ┤ ┬ ┴ ┼ ▫ ■':
+    #     return pos_to
 
     # get direction
     if pos_from.x == pos_to.x:
@@ -94,30 +91,80 @@ def follow_line(mapa: List[List[int]], pos_from: Pos, pos_to: Pos) -> Pos:
         if pos_from.x > pos_to.x:   direction = 'left'
         else:                       direction = 'right'
 
+
+    print(f'from: {pos_from} to: {pos_to}')
+    print(f'from: {mapa[pos_from.y][pos_from.x]} to: {mapa[pos_to.y][pos_to.x]}')
     print(f'direction: {direction}')
 
     pos_final = pos_to
+    iter = 0
 
-    while pos_final not in '├ ┤ ┬ ┴ ┼ ▫ ■':
-        if mapa[pos_to.y][pos_to.x] == '─':
-            if direction == 'left':     pos_final.x -= 1
-            elif direction == 'right':  pos_final.x += 1
-            else: raise Exception('something fucked up')
+    while mapa[pos_final.y][pos_final.x] not in '├ ┤ ┬ ┴ ┼ ▫ ■':
+        iter += 1
+        # print(mapa[pos_final.y][pos_final.x])
+        # print(direction)
+        # print()
+        match mapa[pos_final.y][pos_final.x]:
+            case '─':
+                if direction == 'left':     pos_final.x -= 1
+                elif direction == 'right':  pos_final.x += 1
+                else: raise Exception('something fucked up')
         
-        if mapa[pos_to.y][pos_to.x] == '│':
-            if direction == 'up':       pos_final.y -= 1
-            elif direction == 'down':   pos_final.y += 1
-            else: raise Exception('something fucked up"')
+            case '│':
+                if direction == 'up':       pos_final.y -= 1
+                elif direction == 'down':   pos_final.y += 1
+                else: raise Exception('something fucked up"')
+        
 
+            case '┌':
+                if direction == 'up':       pos_final.x += 1;   direction = 'right'
+                elif direction == 'left':     pos_final.y += 1;   direction = 'down'
+                else: raise Exception('something fucked up"')
+        
+            case '┐':
+                if direction == 'up':       pos_final.x -= 1;   direction = 'left'
+                elif direction == 'right':    pos_final.y += 1;   direction = 'down'
+                else: raise Exception('something fucked up"')
+            
+            case '┘':
+                if direction == 'down':     pos_final.x -= 1;   direction = 'left'
+                elif direction == 'right':    pos_final.y -= 1;   direction = 'up'
+                else: raise Exception('something fucked up"')
+            
+            case '└':
+                if direction == 'down':     pos_final.x += 1;   direction = 'left'
+                elif direction == 'right':    pos_final.y -= 1;   direction = 'up'
+                else: raise Exception('something fucked up"')
 
+            case '▫':
+                pass
+        
+
+    # print(f'final: {pos_final}')
+    return pos_final, iter
 
 
 def main():
     mapa = load_mapa()
     pprint_mapa(mapa)
-    # pos = Pos(1, 0)
-    # print(get_neighbours_near(mapa, pos))
-    follow_line(mapa, Pos(1, 0), Pos(1, 1))
+
+    queue = [Pos(1, 0)]
+
+    while queue != []:
+        pos = queue.pop(-1)
+        print(pos)
+        for neighboor in get_neighbours_near(mapa, pos):
+            print(f'pos:\t\t{pos}')
+            print(f'neighboor:\t{neighboor}')
+            input()
+            queue.append(
+                follow_line(
+                    mapa,
+                    pos,
+                    neighboor,
+                ))
+            
+
 
 
 
