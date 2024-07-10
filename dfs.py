e4f7698bcdcd2e3d5f161b8e452c25170aa228df
Michael36500 <ambros.michael@seznam.cz>
2024-07-10 13:02:50 +0200

    added typing, some work on following the lin

--- dfs.py
+++ dfs.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import *
 
 
 @dataclass
@@ -6,7 +7,7 @@ class Pos:
     x: int
     y: int
 
-def pprint_mapa(mapa):
+def pprint_mapa(mapa: List[List[int]]) -> None:
     for lin in mapa:
         for char in lin:
             print(char, end='')
@@ -17,7 +18,7 @@ def pprint_mapa(mapa):
         print()
 
 
-def load_mapa():
+def load_mapa() -> List[List[int]]:
     mapa = []
     with open('maze') as f:
         for line in f:
@@ -30,7 +31,7 @@ def load_mapa():
     return mapa
 
 
-def get_neighbours(mapa, pos):
+def get_neighbours_near(mapa: List[List[int]], pos: Pos) -> List[Pos]:
     # ─ │ ┌ ┐ ┘ ├ └ ┤ ┬ ┴ ┼ ▫ ■
     current_char = mapa[pos.y][pos.x]
     neighbours_close = []
@@ -73,15 +74,50 @@ def get_neighbours(mapa, pos):
         raise Exception('you are at the end of the maze')
 
 
-    # next node, not turn
-    neig_far = []
+    return neighbours_close
+
+
+def follow_line(mapa: List[List[int]], pos_from: Pos, pos_to: Pos) -> Pos:
+    # ─ │ ┌ ┐ ┘ ├ └ ┤ ┬ ┴ ┼ ▫ ■
+    print(f'from: {pos_from} to: {pos_to}')
+    print(f'from: {mapa[pos_from.y][pos_from.x]} to: {mapa[pos_to.y][pos_to.x]}')
+
+    # if there is not a need to follow a line
+    if mapa[pos_to.y][pos_to.x] in '├ ┤ ┬ ┴ ┼ ▫ ■':
+        return pos_to
+
+    # get direction
+    if pos_from.x == pos_to.x:
+        if pos_from.y > pos_to.y:   direction = 'up'
+        else:                       direction = 'down'
+    else:
+        if pos_from.x > pos_to.x:   direction = 'left'
+        else:                       direction = 'right'
+
+    print(f'direction: {direction}')
+
+    pos_final = pos_to
+
+    while pos_final not in '├ ┤ ┬ ┴ ┼ ▫ ■':
+        if mapa[pos_to.y][pos_to.x] == '─':
+            if direction == 'left':     pos_final.x -= 1
+            elif direction == 'right':  pos_final.x += 1
+            else: raise Exception('something fucked up')
+        
+        if mapa[pos_to.y][pos_to.x] == '│':
+            if direction == 'up':       pos_final.y -= 1
+            elif direction == 'down':   pos_final.y += 1
+            else: raise Exception('something fucked up"')
+
+
 
 
 def main():
     mapa = load_mapa()
     pprint_mapa(mapa)
-    pos = Pos(1, 0)
-    print(get_neighbours(mapa, pos))
+    # pos = Pos(1, 0)
+    # print(get_neighbours_near(mapa, pos))
+    follow_line(mapa, Pos(1, 0), Pos(1, 1))
 
 
 
